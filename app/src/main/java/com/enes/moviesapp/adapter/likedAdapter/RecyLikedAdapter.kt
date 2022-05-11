6f3981a2dd20cf9ec56edf9cde6310f8e33f2bd0
enes <enesselcu25@gmail.com>
2022-05-11 15:58:44 +0300

    update adapter

--- app/src/main/java/com/enes/moviesapp/adapter/likedAdapter/RecyLikedAdapter.kt
+++ app/src/main/java/com/enes/moviesapp/adapter/likedAdapter/RecyLikedAdapter.kt
@@ -28,27 +28,4 @@ class RecyLikedAdapter : ListAdapter<MovieFavoriteEntity, LikedHolder>(BaseDiffU
         holder.likedBind(likedPosition, onClick)
     }
 
-
-//    var movieList : List<MovieFavoriteEntity> = arrayListOf()
-//        set(value) {
-//            field = value
-//            notifyDataSetChanged()
-//        }
-//
-//    private lateinit var movieOnClick: (MovieFavoriteEntity, View) -> Unit
-//    fun onCLik(newMoviesOnclick : (MovieFavoriteEntity, View) ->Unit){
-//        this.movieOnClick = newMoviesOnclick
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikedHolder {
-//        return LikedHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context),
-//            R.layout.item_liked_view,parent,false))
-//    }
-//
-//    override fun onBindViewHolder(holder: LikedHolder, position: Int) {
-//        val moviePosition = movieList[position]
-//        holder.likedBind(moviePosition,movieOnClick)
-//    }
-//
-//    override fun getItemCount(): Int = movieList.size
 }
